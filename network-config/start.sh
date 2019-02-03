5a415c9041d4ced134ec08116383ce2df0aba996
Chainoid Team <chainoid@gmx.com>
2019-02-03 10:15:28 +0200

    Script fix.

--- network-config/start.sh
+++ network-config/start.sh
@@ -13,21 +13,3 @@ export MSYS_NO_PATHCONV=1
 docker-compose -f docker-compose.yml down
 
 docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
-
-# wait for Hyperledger Fabric to start
-# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=12
-#echo ${FABRIC_START_TIMEOUT}
-sleep ${FABRIC_START_TIMEOUT}
-
-# PARSEL
-# Create the channel
-#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c parsel-channel -f /etc/hyperledger/configtx/channel1.tx
-# Join peer0.org1.example.com to the channel.
-#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b parsel-channel.block
-
-# CLIENT
-# Create the channel
-#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c client-channel -f /etc/hyperledger/configtx/channel2.tx
-# Join peer0.org1.example.com to the channel.
-#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b client-channel.block
